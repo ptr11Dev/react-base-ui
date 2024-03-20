719e0a9584bcbde0cc2cd2d9f41010f999a4aeea
kaczmarek.pi <kaczmarek.pi@totalizator.pl>
2024-03-20 13:47:49 +0100

    Change vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+import removeConsole from 'vite-plugin-remove-console'
 
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [react(), dts({ rollupTypes: true }), removeConsole({})],
+  esbuild: {
+    legalComments: 'none',
+    drop: ['console'],
+    treeShaking: true,
+    pure: ['console.log'],
+    minifyIdentifiers: true,
+    minifySyntax: true,
+    minifyWhitespace: true,
+  },
   build: {
+    minify: 'esbuild',
     lib: {
-      entry: "src/index.ts",
-      name: "react-base-ui",
+      entry: 'src/index.ts',
+      name: 'react-base-ui',
       fileName: (format) => `react-base-ui.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom"],
+      external: ['react', 'react-dom'],
       treeshake: true,
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+          react: 'React',
+          'react-dom': 'ReactDOM',
         },
       },
     },
   },
-});
+})
