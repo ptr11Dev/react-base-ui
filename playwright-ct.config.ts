b39cad918dce18bcf16b7af5a0ca26f428fd4eab
kaczmarek.pi <kaczmarek.pi@totalizator.pl>
2024-03-27 10:36:11 +0100

    Add build for production

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -1,8 +1,5 @@
 import { defineConfig, devices } from '@playwright/experimental-ct-react'
 
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: './',
   /* The base directory, relative to the config file, for snapshot files created with toMatchSnapshot and toHaveScreenshot. */
--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import dts from 'vite-plugin-dts'
 import removeConsole from 'vite-plugin-remove-console'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true }), removeConsole({})],
+  plugins: [
+    react(),
+    dts({ rollupTypes: true }),
+    removeConsole({}),
+    visualizer({ open: true, filename: 'bundle-analysis.html' }),
+  ],
   esbuild: {
     legalComments: 'none',
     drop: ['console'],
@@ -22,12 +28,14 @@ export default defineConfig({
       fileName: (format) => `react-base-ui.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'styled-components'],
       treeshake: true,
       output: {
         globals: {
           react: 'React',
           'react-dom': 'ReactDOM',
+          'react/jsx-runtime': 'react/jsx-runtime',
+          'styled-components': 'styled',
         },
       },
     },
